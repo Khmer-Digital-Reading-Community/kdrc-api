e55ad92c38c49642a2ed8d99ec54d0fa7c64a22f
DavinVatanak <e20220234@dtc1.itc.edu.kh>
2026-05-13 08:58:58 +0700

    add ManyToOne relationship to Book

--- src/modules/books/book.entity.ts
+++ src/modules/books/book.entity.ts
@@ -1,32 +1,46 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn, ManyToMany, JoinTable } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToMany,
+  JoinTable,
+  OneToMany,
+} from 'typeorm';
 import { User } from '../users/user.entity';
 import { Category } from '../categories/category.entity';
+import { Chapter } from '../chapters/entities/chapter.entity';
 
 @Entity()
 export class Book {
-    @PrimaryGeneratedColumn('uuid')
-    id!: string;
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
 
-    @Column()
-    title!: string;
+  @Column()
+  title!: string;
 
-    @Column('text')
-    content!: string;
+  @Column('text')
+  content!: string;
 
-    @ManyToOne(() => User, (user) => user.books, {
-        onDelete: 'CASCADE',
-    })
-    author!: User;
+  @ManyToOne(() => User, (user) => user.books, {
+    onDelete: 'CASCADE',
+  })
+  author!: User;
 
-    @CreateDateColumn()
-    createdAt!: Date;
+  @CreateDateColumn()
+  createdAt!: Date;
 
-    @UpdateDateColumn()
-    updatedAt!: Date;
+  @UpdateDateColumn()
+  updatedAt!: Date;
 
-    @ManyToMany(() => Category, (category) => category.books, {
-        cascade: true,
-    })
-    @JoinTable()
-    categories!: Category[];
+  @ManyToMany(() => Category, (category) => category.books, {
+    cascade: true,
+  })
+  @JoinTable()
+  categories!: Category[];
+
+  @OneToMany(() => Chapter, (chapter) => chapter.book)
+  chapters!: Chapter[];
 }
