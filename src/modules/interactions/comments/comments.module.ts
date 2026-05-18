2f5c241544ef8921a17c13cca65fca7c68689390
DavinVatanak <e20220234@dtc1.itc.edu.kh>
2026-05-18 22:47:13 +0700

    .

--- src/modules/interactions/comments/comments.module.ts
+++ src/modules/interactions/comments/comments.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentsService } from './comments.service';
 import { CommentSController } from './comments.controller';
+import { Comment } from './entities/comment.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Comment])], // Connect the enity of the database
