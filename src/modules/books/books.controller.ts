c82a390f9b5826ed237f4c6ab6d37300734317ed
DavinVatanak <e20220234@dtc1.itc.edu.kh>
2026-05-23 11:11:43 +0700

    feat(books): validate route id params with ParseUUIDPipe in books.controller.ts

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,42 +1,54 @@
-import { Controller, Get, Post, Body, Param, UseGuards, Req, Patch, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  UseGuards,
+  Req,
+  Patch,
+  Delete,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { User } from '../users/user.entity';
 
 @Controller('books')
 export class BooksController {
-    constructor(private booksService: BooksService) { }
+  constructor(private booksService: BooksService) {}
 
-    @Get()
-    findAll() {
-        return this.booksService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.booksService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.booksService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.booksService.findOne(id);
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Post()
-    create(@Body() dto: CreateBookDto, @Req() req) {
-        return this.booksService.create(dto, req.user);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(@Body() dto: CreateBookDto, @Req() req: { user: User }) {
+    return this.booksService.create(dto, req.user);
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Patch(':id')
-    update(
-        @Param('id') id: string,
-        @Body() dto: UpdateBookDto,
-        @Req() req,
-    ) {
-        return this.booksService.update(id, dto, req.user);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateBookDto,
+    @Req() req: { user: User },
+  ) {
+    return this.booksService.update(id, dto, req.user);
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Delete(':id')
-    remove(@Param('id') id: string, @Req() req) {
-        return this.booksService.remove(id, req.user);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  remove(@Param('id', ParseUUIDPipe) id: string, @Req() req: { user: User }) {
+    return this.booksService.remove(id, req.user);
+  }
 }
